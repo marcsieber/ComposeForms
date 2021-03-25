19b7c2922c593354a72bcba2d25a59d8286d78c2
Steve Vogel <steve.vogel@hispeed.ch>
2021-03-25 13:24:33 +0100

    Add comments to Observable.kt

--- src/main/kotlin/model/util/Observable.kt
+++ src/main/kotlin/model/util/Observable.kt
@@ -2,17 +2,30 @@ package model.util
 
 import androidx.compose.runtime.mutableStateOf
 
-
+/**
+ * This class provides a value which can be observed. The value is stored as a MutableState and will invoke the UI
+ * to rerender.
+ * Changes are propagated to the listeners only if the value is different to the previous value
+ *
+ * TODO: MAYBE REMOVE AND USE Delegates.observable("")
+ */
 class Observable<T>(initValue: T) {
 
-    private var value = mutableStateOf(initValue)
+    private var value     = mutableStateOf(initValue)
+    private val listeners = mutableListOf<(T) -> Unit>()
 
-    val listeners = mutableListOf<(T) -> Unit>()
-
+    /**
+     * Returns the value
+     * @return value
+     */
     fun getValue(): T{
         return value.value
     }
 
+    /**
+     * Setting the value if the new value is different to the old one.
+     * All the added listeners get notified.
+     */
     fun setValue(newVal: T){
         if(value.value != newVal) {
             value.value = newVal
@@ -20,6 +33,9 @@ class Observable<T>(initValue: T) {
         }
     }
 
+    /**
+     * Adds a function that gets called after an update of the value
+     */
     fun addListener(obs: (T) -> Unit){
         listeners.add(obs)
     }
