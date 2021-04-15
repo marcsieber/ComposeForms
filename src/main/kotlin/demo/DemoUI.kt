6e0aa231f84b9ba4224fdd49ac4ccecc5c74c7da
Steve Vogel <steve.vogel@hispeed.ch>
2021-04-15 11:37:56 +0200

    Cleanup imports

--- src/main/kotlin/demo/DemoUI.kt
+++ src/main/kotlin/demo/DemoUI.kt
@@ -1,11 +1,5 @@
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import demo.UserDefinedModel
 import ui.Form
 
--- src/main/kotlin/ui/Form.kt
+++ src/main/kotlin/ui/Form.kt
@@ -1,13 +1,10 @@
 package ui
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
@@ -16,7 +13,6 @@ import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.unit.dp
 import model.FormModel
 import model.util.attribute.*
-import javax.swing.border.Border
 
 
 class Form {
