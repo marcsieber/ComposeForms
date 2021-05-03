c1b8f7f85466aabbf941b338156e02ff9e76ee49
Steve Vogel <steve.vogel@hispeed.ch>
2021-05-03 14:30:24 +0200

    Lower complexity of Dropdown Element

--- src/main/kotlin/ui/Form.kt
+++ src/main/kotlin/ui/Form.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -215,28 +216,38 @@ class Form {
                         Text(selectionString.value)
                     }
                 }
-                DropdownMenu(
-                    expanded = dropDownIsOpen.value,
-                    onDismissRequest = { dropDownIsOpen.value = false},
-                    modifier = Modifier.wrapContentSize(),
-                    content = {
-                        selectionAttribute.getPossibleSelections().forEachIndexed { index, string ->
-                            val elementIsSelected       = selectionAttribute.getValue()!!.contains(string)
-                            val elementIsSelectedBackgroundColor  = if(elementIsSelected) Color.DarkGray else Color.LightGray
-                            val elementIsSelectedTextColor = if(elementIsSelected) Color.White else Color.Black
-                            DropdownMenuItem(
-                                modifier = Modifier.background(elementIsSelectedBackgroundColor),
-                                onClick = {
-                                    if (!elementIsSelected) {
-                                        selectionAttribute.addUserSelection(string)
-                                    } else { selectionAttribute.removeUserSelection(string)}},
-                                content = {Text( text = string, modifier = Modifier.background(elementIsSelectedBackgroundColor), color = elementIsSelectedTextColor)}
-                            )
-                        }
-                    }
-                )
+                DropDownMenu(dropDownIsOpen, selectionAttribute.getPossibleSelections(), selectionAttribute.getValue()!!,
+                selectionAttribute::addUserSelection, selectionAttribute::removeUserSelection)
             }
 
         }
+    }
+
+
+
+    @Composable
+    fun DropDownMenu(dropDownIsOpen: MutableState<Boolean>, selections: Set<String>, currentSelectionValue: Set<String>,
+                     add :(String)-> Unit, remove : (String) -> Unit) {
+
+        DropdownMenu(
+            expanded = dropDownIsOpen.value,
+            onDismissRequest = { dropDownIsOpen.value = false},
+            modifier = Modifier.wrapContentSize(),
+            content = {
+                selections.forEachIndexed { index, string ->
+                    val elementIsSelected       = currentSelectionValue.contains(string)
+                    val elementIsSelectedBackgroundColor  = if(elementIsSelected) Color.DarkGray else Color.LightGray
+                    val elementIsSelectedTextColor = if(elementIsSelected) Color.White else Color.Black
+                    DropdownMenuItem(
+                        modifier = Modifier.background(elementIsSelectedBackgroundColor),
+                        onClick = {
+                            if (!elementIsSelected) {
+                                add(string)
+                            } else { remove(string)}},
+                        content = {Text( text = string, modifier = Modifier.background(elementIsSelectedBackgroundColor), color = elementIsSelectedTextColor)}
+                    )
+                }
+            }
+        )
     }
 }
