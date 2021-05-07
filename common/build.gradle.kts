2e03c7ea2ec9dc8fa45bfeac057f2cd046bad3eb
Steve Vogel <steve.vogel@hispeed.ch>
2021-05-07 14:20:57 +0200

    clean up

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
-//    kotlin("multiplatform")
     id("org.jetbrains.compose") //version "0.3.0"
     id("jacoco")
     id("org.sonarqube") version "3.1"
@@ -22,57 +21,22 @@ sourceSets{
     }
 }
 
-//kotlin {
-//    jvm {}
-//    sourceSets {
-//        named("commonMain") {
-//            dependencies {
-//                api(compose.runtime)
-//                api(compose.foundation)
-//                api(compose.material)
-//                api("org.junit.jupiter:junit-jupiter:5.7.1")
-//            }
-//        }
-//        named("commonTest") {
-//            dependencies {
-//                api(compose.runtime)
-//                api(compose.foundation)
-//                api(compose.material)
-//                api("org.junit.jupiter:junit-jupiter:5.7.1")
-//            }
-//        }
-//    }
-//
-//}
-
-//dependencies {
-//    implementation(compose.desktop.currentOs)
-//    implementation("org.jetbrains.kotlin:kotlin-reflect")
-//    implementation("org.junit.jupiter:junit-jupiter:5.7.1")
-//}
-
-
-//tasks.withType<Test> {
-//    useJUnitPlatform()
-//}
-
-//tasks {
-//    test {
-//        useJUnitPlatform()
-//    }
-//}
-
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
 }
 
-//tasks{
-//    jacocoTestReport {
-//        reports {
-//            xml.isEnabled = true
-//            csv.isEnabled = false
-//            html.destination = file("${buildDir}/jacocoHtml")
-//        }
-//    }
-//}
+tasks{
+    jacocoTestReport {
+        reports {
+            xml.isEnabled = true
+            csv.isEnabled = false
+            html.destination = file("${buildDir}/jacocoHtml")
+        }
+    }
+}
