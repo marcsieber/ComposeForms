8d8eb72134c567c5401ea9605dedb41eba276ef5
LouisaRe <louisa.reinger@gmx.de>
2021-05-20 11:44:10 +0200

    add entry in gitignore, add import in UI class

--- demo/src/main/kotlin/demo/demo/DemoUI.kt
+++ demo/src/main/kotlin/demo/demo/DemoUI.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import demo.demo.DemoModel
 
 
 @Composable
