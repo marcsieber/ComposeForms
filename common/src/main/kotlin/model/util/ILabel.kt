ec3184cb7213e0e61e0f2951bd4e5b1ce96e58f4
LouisaRe <louisa.reinger@gmx.de>
2021-05-20 13:20:48 +0200

    review language (label/ILabel / Tests)

--- common/src/main/kotlin/model/util/ILabel.kt
+++ common/src/main/kotlin/model/util/ILabel.kt
@@ -4,6 +4,15 @@ import java.lang.reflect.Method
 
 interface ILabel{
 
+    /**
+     * This method returns the label name of an enum constant in the desired language.
+     * If the language is not defined an IllegalArgumentException is thrown
+     *
+     * @param label : Enum<*>
+     * @param lang : String
+     * @return label : String
+     * @throws IllegalArgumentException
+     */
     fun getLabelInLanguage(label: Enum<*>, lang: String): String {
 
         val methodMap = getMethods().map { it.name.removePrefix("get").toLowerCase() to it }.toMap()
@@ -15,11 +24,22 @@ interface ILabel{
         }
     }
 
+    /**
+     * This method can read out all not private parameters (languages) of an enum.
+     * It returns all used languages as a list.
+     *
+     * @return languages : List<String>
+     */
     fun getLanguagesDynamic(): List<String> {
         val methodsFiltered = getMethods()
         return methodsFiltered.map { it.name.removePrefix("get").toLowerCase() }
     }
 
+    /**
+     * This method returns all getter functions that are created in an enum.
+     *
+     * @return getter : List<Method>
+     */
     private fun getMethods(): List<Method> {
 
         val methodsUnfiltered: Array<Method> = this::class.java.methods
