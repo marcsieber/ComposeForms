b4e2b14d758b492b339ab5f69eaabfd8c9793e52
Steve Vogel <steve.vogel@hispeed.ch>
2021-06-23 15:35:43 +0200

    Add import

--- desktop/src/main/kotlin/ui/FormElements.kt
+++ desktop/src/main/kotlin/ui/FormElements.kt
@@ -2,6 +2,7 @@ package ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
